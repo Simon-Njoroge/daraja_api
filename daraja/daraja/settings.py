f367debe786b7a3a74dfe01fd33acf9f12981c85
simon njoroge <mukirisimon22@gmail.com>
2024-11-22 17:26:09 +0300

    daraja api

--- daraja/daraja/settings.py
+++ daraja/daraja/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+ew0ln&w0k&1xx@gitpbko&4!f$x8oynw70=jiwx&$_an3-m$!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
